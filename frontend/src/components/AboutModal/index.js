5d67291c50f03b9e1786ca9b3965e98eb4e0398c
Juanda84 <150093419+Juanda84@users.noreply.github.com>
2025-10-08 19:02:09 -0500

    Update index.js

--- frontend/src/components/AboutModal/index.js
+++ frontend/src/components/AboutModal/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-
 import { makeStyles } from "@material-ui/core/styles";
 import {
   Button,
@@ -24,43 +23,36 @@ const logo = "/vector/logo.svg";
 const logoDark = "/vector/logo-dark.svg";
 
 const useStyles = makeStyles(theme => ({
-	root: {
-		display: "flex",
-		flexWrap: "wrap",
-	},
-	multFieldLine: {
-		display: "flex",
-		"& > *:not(:last-child)": {
-			marginRight: theme.spacing(1),
-		},
-	},
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  multFieldLine: {
+    display: "flex",
+    "& > *:not(:last-child)": {
+      marginRight: theme.spacing(1),
+    },
+  },
   logoImg: {
     width: "100%",
     margin: "0 auto",
     content: `url("${theme.calculatedLogo()}")`
   },
-  ticketzLogoImg: {
-    width: "100%",
-    margin: "0 auto",
-    content: "url(" + (theme.mode === "light" ? logo : logoDark) + ")"
-  },
   textCenter: {
     textAlign: "center",
   }
-
 }));
 
 const AboutModal = ({ open, onClose }) => {
-	const classes = useStyles();
+  const classes = useStyles();
   const { getCurrentUserInfo } = useAuth();
   const [currentUser, setCurrentUser] = useState({});
   const [backendGitInfo, setBackendGitInfo] = useState(null);
   const theme = useTheme();
 
-
-	const handleClose = () => {
-		onClose();
-	};
+  const handleClose = () => {
+    onClose();
+  };
 
   useEffect(() => {
     getCurrentUserInfo().then(
@@ -74,83 +66,86 @@ const AboutModal = ({ open, onClose }) => {
         setBackendGitInfo(response.data);
       }
     )
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-	return (
-		<div className={classes.root}>
-			<Dialog
-				open={open}
-				onClose={handleClose}
-				maxWidth="sm"
-				fullWidth
-				scroll="paper"
-			>
-				<DialogTitle id="form-dialog-title">
-					{i18n.t("about.aboutthe")} {currentUser?.super ? "ticketz" : theme.appName }
-				</DialogTitle>
-				<DialogContent dividers>
-				{ currentUser?.super ? 
+  return (
+    <div className={classes.root}>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        maxWidth="sm"
+        fullWidth
+        scroll="paper"
+      >
+        <DialogTitle id="form-dialog-title">
+          {i18n.t("about.aboutthe")} {theme.appName}
+        </DialogTitle>
+        <DialogContent dividers>
           <>
             <div>
-              <img className={classes.ticketzLogoImg} />
+              <img className={classes.logoImg} alt="Logo" />
             </div>
-            <Typography variant="body1" gutterBottom><b>Frontend: 
-              { frontendGitInfo.tagName && `Version: ${frontendGitInfo.tagName} Build info: ${frontendGitInfo.buildTimestamp}` }
-              { !frontendGitInfo.tagName &&
+            <Typography variant="body1" gutterBottom>
+              <b>Frontend: 
+                { frontendGitInfo.tagName && `Version: ${frontendGitInfo.tagName} Build info: ${frontendGitInfo.buildTimestamp}` }
+                { !frontendGitInfo.tagName &&
+                  <>
+                    {frontendGitInfo.commitHash && `Commit: ${frontendGitInfo.commitHash} `}
+                    {frontendGitInfo.branchName && `Branch: ${frontendGitInfo.branchName} `}
+                    {frontendGitInfo.commitTimestamp && `Time: ${frontendGitInfo.commitTimestamp} `}
+                  </>
+                }
+              </b>
+              {backendGitInfo &&
                 <>
-                  {frontendGitInfo.commitHash && `Commit: {frontendGitInfo.commitHash} `}
-                  {frontendGitInfo.branchName && `Branch: {frontendGitInfo.branchName} `}
-                  {frontendGitInfo.commitTimestamp && `Time: {frontendGitInfo.commitTimestamp} `}
+                  <br /><b>Backend: 
+                    { backendGitInfo.tagName && `Version: ${backendGitInfo.tagName} Build info: ${backendGitInfo.buildTimestamp}` }
+                    { !backendGitInfo.tagName &&
+                      <>
+                        {backendGitInfo.commitHash && `Commit: ${backendGitInfo.commitHash} `}
+                        {backendGitInfo.branchName && `Branch: ${backendGitInfo.branchName} `}
+                        {backendGitInfo.commitTimestamp && `Time: ${backendGitInfo.commitTimestamp} `}
+                      </>
+                    }
+                  </b>
                 </>
               }
-            </b>
-            {backendGitInfo &&
-            <>
-            <br /><b>Backend: 
-              { backendGitInfo.tagName && `Version: ${backendGitInfo.tagName} Build info: ${backendGitInfo.buildTimestamp}` }
-              { !backendGitInfo.tagName &&
-                <>
-                  {backendGitInfo.commitHash && `Commit: {backendGitInfo.commitHash} `}
-                  {backendGitInfo.branchName && `Branch: {backendGitInfo.branchName} `}
-                  {backendGitInfo.commitTimestamp && `Time: {backendGitInfo.commitTimestamp} `}
-                </>
-              }
-            </b>
-            </>
-            }
             </Typography>
             <Typography variant="body1">{i18n.t("about.aboutdetail")}</Typography>
-            <Typography><Link target="_blank" href="https://todobom.com">{i18n.t("about.aboutauthorsite")}</Link></Typography>
-            <Typography><Link target="_blank" href="https://github.com/canove/whaticket-community">{i18n.t("about.aboutwhaticketsite")}</Link></Typography>
-            <Typography><Link target="_blank" href="https://github.com/vemfazer">{i18n.t("about.aboutvemfazersite")}</Link></Typography>
+            <Typography>
+              <Link target="_blank" href="https://yourbrand.com">
+                {i18n.t("about.aboutauthorsite")}
+              </Link>
+            </Typography>
+            <Typography>
+              <Link target="_blank" href="https://github.com/your-org/your-repo">
+                {i18n.t("about.licensesourcecode")}
+              </Link>
+            </Typography>
             <Typography variant="h4">{i18n.t("about.licenseheading")}</Typography>
             <Typography variant="body1">{i18n.t("about.licensedetail")}</Typography>
-            <Typography><Link target="_blank" href="https://github.com/ticketz-oss/ticketz/blob/main/LICENSE.md">{i18n.t("about.licensefulltext")}</Link></Typography>
-            <Typography><Link target="_blank" href="https://github.com/ticketz-oss/ticketz">{i18n.t("about.licensesourcecode")}</Link></Typography>
+            <Typography>
+              <Link target="_blank" href="https://github.com/your-org/your-repo/blob/main/LICENSE.md">
+                {i18n.t("about.licensefulltext")}
+              </Link>
+            </Typography>
           </>
-          :
-          <>
-            <div>
-              <img className={classes.logoImg} />
-            </div>
-            <Typography className={classes.textCenter} ><Link target="_blank" href="https://ticke.tz">{i18n.t("about.copyright")}</Link></Typography>
-          </>
-        }
-				</DialogContent>
-				<DialogActions>
-					<Button
-						onClick={handleClose}
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleClose}
             type="submit"
             color="primary"
             variant="contained"
-					>
-						{i18n.t("about.buttonclose")}
-					</Button>
-				</DialogActions>
-			</Dialog>
-		</div>
-	);
+          >
+            {i18n.t("about.buttonclose")}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 };
 
 export default AboutModal;
+
